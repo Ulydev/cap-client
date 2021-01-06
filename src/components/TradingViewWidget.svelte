2053a7d71e1858cca5806464070ed0b44a5db802
ulydev <ulydev@live.fr>
2021-01-07 00:48:09 +0100

    set up tradingview widget

---
+++ src/components/TradingViewWidget.svelte
@@ -0,0 +1,63 @@
+<script>
+    import { onMount } from "svelte";
+    export let options = Object;
+    const SCRIPT_ID = "tradingview-widget-script";
+    let CONTAINER_ID = "";
+
+    let widget = undefined;
+  
+    onMount(() => {
+        CONTAINER_ID =
+            options && options.container_id
+            ? options.container_id
+            : "svelte-tradingview-widget;";
+        appendScript(initWidget);
+    });
+  
+    function initWidget() {
+        if (typeof TradingView !== "undefined") {
+            widget = new window.TradingView.widget(
+                Object.assign({ container_id: CONTAINER_ID }, options)
+            );
+            widget.postMessage.post("setLogoOverlayVisibility", false)
+            return true
+        }
+        return false
+    }
+  
+    function appendScript(onload) {
+        if (document.getElementById(SCRIPT_ID) === null) {
+            const script = document.createElement("script");
+            script.id = SCRIPT_ID;
+            script.type = "text/javascript";
+            script.async = true;
+            script.src = "https://s3.tradingview.com/tv.js";
+            script.onload = onload;
+            document.getElementsByTagName("head")[0].appendChild(script);
+        } else {
+            const script = document.getElementById(SCRIPT_ID);
+            const oldOnload = script.onload;
+            return (script.onload = () => {
+                oldOnload();
+                onload();
+            });
+        }
+    }
+
+    function updateOptions() {
+        if (!initWidget())
+            appendScript(initWidget)
+    }
+    
+    $: autosize = options.autosize
+    $: updateOptions(options)
+</script>
+  
+<div id={CONTAINER_ID} class:autosize />
+  
+<style>
+    .autosize {
+        width: 100%;
+        height: 100%;
+    }
+</style>
--- src/lib/products.js
+++ src/lib/products.js
@@ -6,6 +6,11 @@ const PRODUCT_TO_FIGI = {
 	"PFE": "BBG000BR2B91"
 };
 
+const PRODUCT_TO_TRADINGVIEW_ID = {
+	"BTC": "BTCUSD",
+	"ETH": "ETHUSD"
+}
+
 let FIGI_TO_PRODUCT = {};
 for (const p in PRODUCT_TO_FIGI) {
 	const figi = PRODUCT_TO_FIGI[p];
@@ -18,4 +23,8 @@ export function figiToProduct(figi) {
 
 export function productToFigi(product) {
 	return PRODUCT_TO_FIGI[product] || product;
+}
+
+export function productToTradingViewID(product) {
+	return PRODUCT_TO_TRADINGVIEW_ID[product] || product;
 }
