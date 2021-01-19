e78f4a8870e54e4b2bf487d0238759aae82d6b3c
ulydev <ulydev@live.fr>
2021-01-19 13:37:23 +0100

    add Cap color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 // tailwind.config.js
 const production = !process.env.ROLLUP_WATCH; // or some other env var like NODE_ENV
+
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   future: { // for tailwind 2.0 compat
     purgeLayersByDefault: true, 
@@ -18,7 +21,30 @@ module.exports = {
   },
   variants: {
     extend: {
-      display: ["dark"]
+      display: ["dark"],
+      opacity: ["dark"],
+      backgroundOpacity: ["dark"]
+    }
+  },
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      gray: colors.warmGray,
+      white: colors.white,
+      primary: {
+        100: "#43ff44",
+        200: "#39dc38",
+        300: "#2fba2c",
+        400: "#249820"
+      },
+      secondary: {
+        100: "#c5c5c7",
+        200: "#25254f",
+        300: "#1e1e44",
+        400: "#1d1c3d"
+      }
     }
   },
   darkMode: 'class',
