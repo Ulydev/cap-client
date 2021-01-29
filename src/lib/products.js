51b5a183f5b56ece99439134c77913a88ed0405e
ulydev <ulydev@live.fr>
2021-01-29 01:57:59 +0100

    create products list with icons

--- src/lib/products.js
+++ src/lib/products.js
@@ -1,21 +1,21 @@
-const PRODUCT_TO_FIGI = {
-	"BTC": "BTC",
-	"ETH": "ETH",
-	"AAPL": "BBG000B9XRY4",
-	"TSLA": "BBG000N9MNX3",
-	"PFE": "BBG000BR2B91",
-	"FB": "BBG000MM2P62",
-	"NFLX": "BBG000CL9VN6",
-	"GOOG": "BBG009S3NB30",
-	"SP500": "BBG000H4FSM0"
-};
+export const products = [
+	{ symbol: "BTC",	figi: "BTC",			name: "Bitcoin",	tv: "BTCUSD" },
+	{ symbol: "ETH",	figi: "ETH",			name: "Ethereum",	tv: "ETHUSD" },
+	{ symbol: "LINK",	figi: "LINK",			name: "Chainlink",	tv: "LINKUSD" },
+	{ symbol: "AAPL",	figi: "BBG000B9XRY4",	name: "Apple",		},
+	{ symbol: "TSLA",	figi: "BBG000N9MNX3",	name: "Tesla",		},
+	//{ symbol: "PFE",	figi: "BBG000BR2B91",	name: "Pfizer",		},
+	{ symbol: "FB",		figi: "BBG000MM2P62",	name: "Facebook",	},
+	{ symbol: "NFLX",	figi: "BBG000CL9VN6",	name: "Netflix",	},
+	{ symbol: "GOOG",	figi: "BBG009S3NB30",	name: "Google",		},
+	{ symbol: "SP500",	figi: "BBG000H4FSM0",	name: "S&P 500",	tv: "SP:SPX" },
+]
 
-const PRODUCT_TO_TRADINGVIEW_ID = {
-	"BTC": "BTCUSD",
-	"ETH": "ETHUSD",
-	"LINK": "LINKUSD",
-	"SP500": "SP:SPX"
-}
+const PRODUCT_TO_FIGI = {}
+products.filter(p => !!p.figi).forEach(p => { PRODUCT_TO_FIGI[p.symbol] = p.figi })
+
+const PRODUCT_TO_TRADINGVIEW_ID = {}
+products.filter(p => !!p.tv).forEach(p => { PRODUCT_TO_TRADINGVIEW_ID[p.symbol] = p.tv })
 
 let FIGI_TO_PRODUCT = {};
 for (const p in PRODUCT_TO_FIGI) {
